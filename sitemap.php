2d14885daec758bd3c938331eedb6f3809370bd8
Josh Richard Dyke <jrdyke@silk2.uvm.edu>
2015-10-26 09:27:34 -0400

    added advising

--- sitemap.php
+++ sitemap.php
@@ -17,6 +17,7 @@
             <li><a href="assignment1.0/tables.php">Assignment 1.0</a></li>
             <li><a href="assignment2.0/select.php">Assignment 2.0</a></li>
             <li><a href="assignment3.0/join.php">Assignment 3.0</a></li>
+            <li><a href="advising/index.php">Advising</a></li>
             <li><a href="https://www.google.com/">www.google.com</a></li>
         </ul>
     </body>
