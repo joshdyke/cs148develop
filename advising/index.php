444c50cb70bc07805e8ade40963a0596b8647f03
Josh Richard Dyke <jrdyke@silk2.uvm.edu>
2015-11-04 11:52:59 -0500

    index.php edits, 4yp testing template

--- advising/index.php
+++ advising/index.php
@@ -20,142 +20,62 @@ if (isset($_GET['getRecordsFor'])) {
 print '<article>';
 print '<h2>Database: ' . DATABASE_NAME . '</h2>';
 
+//get four year plan
+print '<h1>Four Year Plan</h1>';
+
+$columns = 2;
+    //now print out each record
+    $query = 'SELECT tblAdvisor.pmkNetId, tblStudent.pmkNetId ' .
+             'FROM tblAdvisor, tblStudent';
+
+//    $topResults = $thisDatabaseReader->testQuery($query, "", 0, 0, 0, 0, false, false);
+    $topResults = $thisDatabaseReader->select($query, "", 0, 0, 0, 0, false, false);
+
+    foreach ($topResults as $rec) {
+        for ($i = 0; $i < $columns; $i++) {
+            print '<p>' . $rec[$i] . '</td>';
+        }
+    }
+
 // print out a list of all the tables and their description
 // make each table name a link to display the record
 print '<section id="tables2" class="float_left">';
 
-//get four year plan
-print '<a href="">My Plan</a>';
 
-$columns = 17;
-    //now print out each record
-    $query = 'SELECT * ' .
-             'FROM tbl4YP ' .
-             'JOIN tblSemesterPlan ON tbl4YP.pmkPlanId=tblSemesterPlan.fnkPlanId ' .
-             'JOIN tblSemester ON tblSemesterPlan.fnkPlanId=tblSemester.fnkPlanId ' .
-             'AND tblSemesterPlan.fnkTerm=tblSemester.fnkTerm ' . 
-             'AND tblSemesterPlan.fnkYear=tblSemester.fnkYear ' .
-             'JOIN tblCourses ON tbl4YP.fldMajor=tblCourses.fldDept';
-
-    $results = $thisDatabaseReader->testQuery($query, "", 0, 2, 0, 0, false, false);
-    $results = $thisDatabaseReader->select($query, "", 0, 2, 0, 0, false, false);
     
-    print '<table class ="Test Four Year Plan">';
-    print '<tr><th>Wrong</th><th>Wrong</th><th>Wrong</th><th>Wrong</th><th>Wrong</th></tr>';
+    
+$columns = 4;
+    //now print out each record
+    $query = 'SELECT tblSemester.fnkTerm, tblSemester.fnkYear, fldDept, fldCourseNum ' . 
+             'FROM tblCourses ' . 
+             'JOIN tblSemester ON  tblSemester.fnkCourseId=tblCourses.pmkCourseId ' .
+             'JOIN tblSemesterPlan ON tblSemesterPlan.fnkPlanId=tblSemester.fnkPlanId ' .
+             'JOIN tbl4YP ON tbl4YP.pmkPlanId=tblSemesterPlan.fnkPlanId ' . 
+             'JOIN tblStudent ON tblStudent.pmkNetId=tbl4YP.pmkStudentId ' .
+             'WHERE tbl4YP.pmkPlanId= "1" AND tblSemesterPlan.fnkTerm="Fall" AND tblSemesterPlan.fnkYear="1"';
+
+//    $results = $thisDatabaseReader->testQuery($query, "", 1, 2, 6, 0, false, false);
+    $results = $thisDatabaseReader->select($query, "", 1, 2, 6, 0, false, false);
+    
+    $totalCredits = 0;
+    $semCredits = 0;
+    $semester = "";
+    
+    
     foreach ($results as $rec) {
         
-        print '<tr class="">';
-        for ($i = 0; $i < $columns; $i++) {
-            print '<td>' . $rec[$i] . '</td>';
+        print '<h1> ' . $rec[0] . ' ' . $rec[1] . '</h1>';
+        $semester = $rec[0] . ' ' . $rec[1];
+        
+        print '<ol>';
+        for ($i = 2; $i < $columns; $i++) {
+            print '<li>' . $rec[$i] . '</li>';
+            
         }
-        print '</tr>';
+        print '</ol>';
     }
 
-    // all done
-    print '</table>';
 
-
-//print '<table>';
-//
-//$query = 'SHOW TABLES';
-//
-//$results = $thisDatabaseReader->select($query, "", 0, 0, 0, 0, false, false);
-//
-//// loop through all the tables in the database, display fields and properties
-//foreach ($results as $row) {
-//
-//    // table name link
-//    print '<tr class="odd">';
-//    print '<th colspan="6">';
-//    print '<a href="?getRecordsFor=' . htmlentities($row[0], ENT_QUOTES) . "#" . htmlentities($row[0], ENT_QUOTES) . '">';
-//    print htmlentities($row[0], ENT_QUOTES) . '</a>';
-//    print '</th>';
-//    print '</tr>';
-//
-//    //get the fields and any information about them
-//    $query = 'SHOW COLUMNS FROM ' . $row[0];
-//    $results2 = $thisDatabaseReader->select($query, "", 0, 0, 0, 0, false, false);
-//
-//    foreach ($results2 as $row2) {
-//        print '<tr>';
-//        print '<td>' . $row2['Field'] . '</td>';
-//        print '<td>' . $row2['Type'] . '</td>';
-//        print '<td>' . $row2['Null'] . '</td>';
-//        print '<td>' . $row2['Key'] . '</td>';
-//        print '<td>' . $row2['Default'] . '</td>';
-//        print '<td>' . $row2['Extra'] . '</td>';
-//        print '</tr>';
-//    }
-//}
-//print '</table>';
-//print '</section>';
-//
-//// Display all the records for a given table
-//if ($tableName != "") {
-//    print '<aside id="records">';
-//
-//    $query = 'SHOW COLUMNS FROM ' . $tableName;
-//    $info = $thisDatabaseReader->select($query,  "", 0, 0, 0, 0, false, false);
-//
-//    $span = count($info);
-//
-//    //print out the table name and how many records there are
-//    print '<table>';
-//
-//    $query = 'SELECT * FROM ' . $tableName;
-//    $a = $thisDatabaseReader->select($query,  "", 0, 0, 0, 0, false, false);
-//
-//    print '<tr>';
-//    print '<th colspan=' . $span . '>' . $query;
-//    print '</th>';
-//    print '</tr>';
-//
-//    print '<tr>';
-//    print '<th colspan=' . $span . '>' . $tableName;
-//    print ' ' . count($a) . ' records';
-//    print '</th>';
-//    print '</tr>';
-//
-//    // print out the column headings, note i always use a 3 letter prefix
-//    // and camel case like pmkCustomerId and fldFirstName
-//    print '<tr>';
-//    $columns = 0;
-//    foreach ($info as $field) {
-//        print '<td>';
-//        $camelCase = preg_split('/(?=[A-Z])/', substr($field[0], 3));
-//
-//        foreach ($camelCase as $one) {
-//            print $one . " ";
-//        }
-//
-//        print '</td>';
-//        $columns++;
-//    }
-//    print '</tr>';
-//
-//    //now print out each record
-//    $query = 'SELECT * FROM ' . $tableName;
-//    $info2 = $thisDatabaseReader->select($query, "", 0, 0, 0, 0, false, false);
-//
-//    $highlight = 0; // used to highlight alternate rows
-//    foreach ($info2 as $rec) {
-//        $highlight++;
-//        if ($highlight % 2 != 0) {
-//            $style = ' odd ';
-//        } else {
-//            $style = ' even ';
-//        }
-//        print '<tr class="' . $style . '">';
-//        for ($i = 0; $i < $columns; $i++) {
-//            print '<td>' . $rec[$i] . '</td>';
-//        }
-//        print '</tr>';
-//    }
-//
-//    // all done
-//    print '</table>';
-//    print '</aside>';
-//}
 print '</article>';
 include "footer.php";
 ?>
